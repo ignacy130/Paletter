839d92b18ebce540eff039e9de2dd3ed4e945a53
Ignacy <ignacy130@gmail.com>
2016-06-09 11:14:42 +0200

    count vector length

---
+++ Paletter/MathHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Paletter
+{
+    public class MathHelpers
+    {
+        public static double GetVectorLength(double[] vector)
+        {
+            var squaresSum = vector.Select(x => x * x).Sum();
+            var result = Math.Sqrt(squaresSum);
+            return result;
+        }
+    }
+}
